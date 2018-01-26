2936fb64d2ec16de7cfc8744adb23c20cedf0f8c
xarmykwon <lion_kwon@naver.com>
2018-01-26 15:25:37 +0900

    최초 업로

--- rxjava/src/main/java/com/lionkwon/jxjava/FirstExample.java
+++ rxjava/src/main/java/com/lionkwon/jxjava/FirstExample.java
@@ -1,15 +1,32 @@
 package com.lionkwon.jxjava;
 
 import io.reactivex.Observable;
+import io.reactivex.schedulers.Schedulers;
 
 public class FirstExample {
 
 	public void emit() {
-		Observable.just("hello", "Rxjava21").subscribe(System.out::println);
+		Observable.just(
+				"hello", "Rxjava21", "Rxjava22", "Rxjava23",
+				"hello", "Rxjava21", "Rxjava22", "Rxjava23",
+				"hello", "Rxjava21")
+		.subscribe(System.out::println);
+		
+		
+		Observable.just("bring", "man", 1, 2, 3, 4, 5, 6)
+		.subscribeOn(Schedulers.computation())
+		.subscribe(data->System.out.println(Thread.currentThread().getName()+":"+data));
+		
+		Observable.just("brin2", "ma2", 1, 2, 3, 4, 5, 6)
+		.subscribeOn(Schedulers.computation()) //
+		.observeOn(Schedulers.newThread())
+		.subscribe(data->System.out.println(Thread.currentThread().getName()+":"+data));
 	}
 	
+		
 	public static void main(String args[]) {
 		FirstExample demo = new FirstExample();
 		demo.emit();
+		
 	}
 }
